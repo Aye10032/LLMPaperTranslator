7e2e840ed35ee4218f390670c9f54e199675bceb
Aye10032 <aye10032@gmail.com>
2024-03-18 19:00:10 +0800

    初始窗体

---
+++ Main.py
@@ -0,0 +1,32 @@
+# enable dpi scale
+import os
+import sys
+
+from PySide6.QtCore import Qt, QTranslator
+from PySide6.QtWidgets import QApplication
+from qfluentwidgets import FluentTranslator
+
+from common.Config import cfg
+from view.MainWindow import MainWindow
+
+if cfg.get(cfg.dpiScale) != "Auto":
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = str(cfg.get(cfg.dpiScale))
+
+# create application
+app = QApplication(sys.argv)
+app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
+
+# internationalization
+locale = cfg.get(cfg.language).value
+translator = FluentTranslator(locale)
+galleryTranslator = QTranslator()
+
+app.installTranslator(translator)
+app.installTranslator(galleryTranslator)
+
+# create main window
+w = MainWindow()
+w.show()
+
+app.exec()
---
+++ view/MainWindow.py
@@ -0,0 +1,9 @@
+from qfluentwidgets import FluentWindow
+
+
+class MainWindow(FluentWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # self.initWindow()
